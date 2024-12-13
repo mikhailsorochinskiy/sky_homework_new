ca354ff59bb617e998788d99dc1b402250da8b24
Michael <sorokajr678@bk.ru>
2024-12-13 22:54:29 +0300

    add sort_by_date func

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,8 @@ def filter_by_state(id_list: list, state: str = "EXECUTED") -> list:
             new_id_list.append(items)
 
     return new_id_list
+
+
+def sort_by_date(id_list: list, sort_option: bool = True) -> list:
+    """Функция сортирует список словарей по датам в порядке убывания"""
+    return sorted(id_list, key=lambda x: x["date"], reverse=sort_option)
