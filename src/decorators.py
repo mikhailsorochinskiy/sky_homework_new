18bf1ea9613960a2341be0089e3cc40396b1a08a
Michael <sorokajr678@bk.ru>
2025-01-09 00:57:39 +0300

    fixes

--- src/decorators.py
+++ src/decorators.py
@@ -1,31 +1,28 @@
+from functools import wraps
+
+
 def log(filename=""):
     def decorator(func):
+        @wraps(func)
         def wrapper(*args, **kwargs):
             try:
                 func(*args, **kwargs)
             except Exception as e:
-                result = f'{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}'
+                result = f"{func.__name__} error: {type(e).__name__}. Inputs: {args}, {kwargs}"
                 if not filename:
                     print(result)
                 else:
-                    with open(filename, 'w') as file:
+                    with open(filename, "w") as file:
                         file.write(result)
                 return result
-            result = f'{func.__name__} {func(*args, **kwargs)}'
+            result = f"{func.__name__} {func(*args, **kwargs)}"
             if not filename:
                 print(result)
             else:
-                with open(filename, 'w') as file:
+                with open(filename, "w") as file:
                     file.write(result)
             return result
+
         return wrapper
+
     return decorator
-
-@log(filename="")
-def get_mask_account(account_number: str) -> str:
-    """Номер счета принимает вид: **XXXX (X - цифра)"""
-    if len(account_number) != 20 or account_number.isdigit() is False:
-        raise ValueError('Некорректное значение')
-    return "**" + account_number[-4:]
-
-print(get_mask_account('12345678'))
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,39 +1,32 @@
 from src.decorators import log
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_divide():
-    @log(filename='')
+    @log(filename="")
     def divide(a, b):
         return a / b
 
     result = divide(3, 0)
-    assert result == 'divide error: ZeroDivisionError. Inputs: (3, 0), {}'
+    assert result == "divide error: ZeroDivisionError. Inputs: (3, 0), {}"
 
 
 def test_add():
-    @log(filename='mylog.txt')
+    @log(filename="mylog.txt")
     def add_args(a, b):
         return a + b
 
     result = add_args(1, 2)
-    assert result == 'add_args 3'
+    assert result == "add_args 3"
 
-# def sss():
-#     return get_mask_card_number
 
 def test_get_mask_card_number_with_decorator():
-    @log(filename='')
-    # @wraps(get_mask_card_number)
-    def get_mask_card_number_with_decorator():
-        return get_mask_card_number('1234567887654321')
-
-    assert get_mask_card_number_with_decorator() == 'get_mask_card_number_with_decorator 1234 56** **** 4321'
+    decorated_function = log(filename="")(get_mask_card_number)
+    result = decorated_function("1234567887654321")
+    assert result == "get_mask_card_number 1234 56** **** 4321"
 
 
 def test_get_mask_account_with_decorator():
-    @log(filename='')
-    def get_mask_account_with_decorator():
-       return get_mask_account('1234567887654321')
-    assert get_mask_account_with_decorator() == ('get_mask_account_with_decorator error: ValueError. Inputs: '
-                                                 '(), {}')
+    decorated_function = log(filename="")(get_mask_account)
+    result = decorated_function("1234567887654321")
+    assert result == "get_mask_account error: ValueError. Inputs: ('1234567887654321',), {}"
