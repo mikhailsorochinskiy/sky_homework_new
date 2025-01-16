2ad1a025939f6b44e42440607a91f794412b65bc
Michael <sorokajr678@bk.ru>
2025-01-16 21:59:34 +0300

    add utils.py, external_api.py, .env.example

---
+++ src/utils.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+
+def get_operations_from_json(file: str) -> list[dict]:
+    """Функция принимает json-файл и возвращает список словарей о транзакциях"""
+    if not os.path.isfile(file):
+        print(f"Файл не найден: {file}")
+        return []
+    with open(file, encoding="utf-8") as f:
+        operations = json.load(f)
+    if type(operations) is not list:
+        return []
+    return operations
+
+
+print(get_operations_from_json("../data/operations.json1"))
