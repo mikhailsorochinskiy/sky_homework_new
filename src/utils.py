6088751c9a7e71df99c0397dad45135b73251aa0
Michael <sorokajr678@bk.ru>
2025-01-17 20:47:47 +0300

    fixes utils.py, test_utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,17 @@
 import json
-import os
 
 
 def get_operations_from_json(file: str) -> list[dict]:
     """Функция принимает json-файл и возвращает список словарей о транзакциях"""
-    if not os.path.isfile(file):
-        print(f"Файл не найден: {file}")
+    try:
+        with open(file, encoding="utf-8") as f:
+            operations = json.load(f)
+        if type(operations) is not list:
+            return []
+        return operations
+    except json.JSONDecodeError:
+        print(f"Ошибка декодирования JSON: {file}")
         return []
-    with open(file, encoding="utf-8") as f:
-        operations = json.load(f)
-    if type(operations) is not list:
+    except FileNotFoundError:
+        print(f"Файл {file} не найден")
         return []
-    return operations
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,14 +34,12 @@ operations_test = """[{
 
 mocked_open = mock_open(read_data=operations_test)
 
-with patch("builtins.open", mocked_open):
-    # Здесь вызывай свою функцию, которая читает JSON-файл
-    result = get_operations_from_json("путь_к_файлу")
-
 
 def test_get_operations_from_json():
-    """Положительный тест"""
-    assert get_operations_from_json("путь_к_файлу") == result
+    """ Положительный тест"""
+    with patch('builtins.open', mocked_open):
+        result = get_operations_from_json('../data/operations.json')
+        assert get_operations_from_json('../data/operations.json') == result
 
 
 def test_get_operations_from_json_invalid_path():
@@ -52,3 +50,26 @@ def test_get_operations_from_json_invalid_path():
 def test_get_operations_from_json_not_list():
     """Файл empty.json пустой файл, то есть тип данных None, а не List"""
     assert get_operations_from_json("../data/empty.json") == []
+
+
+invalid_json = """[{
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+      "amount": "31957.58",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    }
+"""
+
+
+mocked_open_invalid_json = mock_open(read_data=invalid_json)
+
+
+def test_get_operations_from_invalid_json():
+    with patch('builtins.open', mocked_open_invalid_json):
+        result = get_operations_from_json('../data/operations.json')
+        assert get_operations_from_json('../data/operations.json') == result
